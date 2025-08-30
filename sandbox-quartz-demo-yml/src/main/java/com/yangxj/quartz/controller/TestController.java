fe39b2f1aeda0cc4b653c5e8bb274f8c64ea31f1
jansenzhao <zhaoyansheng163@163.com>
2025-08-30 08:18:15 +0800

    1

---
+++ sandbox-quartz-demo-yml/src/main/java/com/yangxj/quartz/controller/TestController.java
@@ -0,0 +1,42 @@
+package com.yangxj.quartz.controller;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * @author yangxj
+ * @date 2019/5/16-21:45
+ * 注意：一个任务JOB可以添加多个Trigger 但是一个Trigger只能绑定一个JOB 这点需要注意
+ */
+@Slf4j
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public String getTest()  {
+        log.info("enter test");
+        System.out.println("enter test");
+        return "1111";
+    }
+    @RequestMapping("/testlist")
+    public List<String> getList() {
+        log.info("enter getList");
+        System.out.println("enter getList");
+        List<String> a = new ArrayList<>();
+        a.add("11");
+        a.add("test1");
+        return a;
+    }
+
+    @RequestMapping("/getAge")
+    public Integer getAge()  {
+        log.info("enter getAge");
+        System.out.println("enter getAge");
+        return 1;
+    }
+}
